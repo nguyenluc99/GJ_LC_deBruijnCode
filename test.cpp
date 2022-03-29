edc3bcfb8490b4b27c36a949c8e3445ef8314606
nguyenluc99 <yeuma99@gmail.com>
2022-03-29 21:47:48 +0700

    add test case for equation formation

--- test.cpp
+++ test.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>    // std::copy
 #include "util.hpp"
 #include "solver.hpp"
 #include "test.hpp"
@@ -17,6 +18,42 @@ bool vectorCompare(string a, string b) {
   return true;
 }
 
+template <typename T>
+bool equationCompare(vector<vector<vector<T> > > formed, vector<vector<vector<T> > > desired) {
+  if (formed.size() != desired.size()) return false;
+  vector<vector<T> > formedRow, desiredRow;
+  for (int i = 0; i < formed.size(); i++) {
+    formedRow = formed[i];
+    desiredRow = desired[i];
+    if (formedRow.size() != desiredRow.size()) {
+      cout << "FAILED AT " << i << endl;
+      return false;
+    }
+
+    for (int j = 0; j < formedRow.size(); j++) 
+      if (!vectorCompare(formedRow[j], desiredRow[j])) {
+        cout << "FAILED AT " << i << " and " << j << endl;
+        return false;
+      }
+  }
+  return true;
+}
+
+
+template <typename T>
+vector<T> getFirstHalf(vector<T> myVec) {
+  //vector<T>::const_iterator 
+    auto first = myVec.begin();
+  //vector<T>::const_iterator 
+    auto last = myVec.begin() + myVec.size() / 2;
+  vector<T> newVec(first, last);
+  return newVec;
+}
+
+bool compareBinStr(string s1, string s2) {
+  return s1.length() < s2.length();
+}
+
 
 void testEquasFormation() {
   cout << "Start testing bitReverse" << endl;
@@ -30,8 +67,48 @@ void testEquasFormation() {
   ret = overlap("0000", "0000"); 
   vector<coefdt> expect(4, 1);
   assert(vectorCompare(ret, expect));
-  //assert(overlap("0000", "0101") == {0});
-  //assert(overlap("1111", "0101") == {0,0,0,1});
-
+  ret = overlap("0000", "0101"); 
+  expect = {0,0,0,1};
+  assert(vectorCompare(ret, expect));
+  ret = overlap("0101", "1010"); 
+  expect = {0,1,0,1};
+  assert(vectorCompare(ret, expect));
+  ret = overlap("0101", "1111"); 
+  expect = {0,0,0,1};
+  assert(vectorCompare(ret, expect));
   cout << "Done testing overlap" << endl;
+
+  cout << "Start testing Equation formation" << endl;
+  int b = 3, k = 2;
+  vector<vector<vector<coefdt> > > equations = initb3k2();
+  vector<string> wordList = genWordList(b, k);
+  vector<string> halfWordList = getFirstHalf(wordList);//(&wordList[0], &wordList[0] + wordList.size() / 2);
+  vector<vector<vector<coefdt> > > formedEquas = genEquation(halfWordList);
+  assert(equationCompare(formedEquas, equations));
+  cout << "PASS b=" << b << ", k=" << k << endl;
+
+  b = 4; k = 3;
+  equations = initb4k3_new();
+  wordList = genWordList(b, k);
+  halfWordList = getFirstHalf(wordList);
+  sort(halfWordList.begin(), halfWordList.end(), compareBinStr);
+  formedEquas = genEquation(halfWordList);
+  assert(equationCompare(formedEquas, equations));
+  cout << "PASS b=" << b << ", k=" << k << endl;
+
+  b = 4; k = 4;
+  equations = initb4k4();
+  wordList = genWordList(b, k);
+  halfWordList = getFirstHalf(wordList);
+  sort(halfWordList.begin(), halfWordList.end(), compareBinStr);
+  formedEquas = genEquation(halfWordList);
+  // cout << "========== FORMED: " << endl;
+  // printMatrix(formedEquas);
+  // cout << "========== DESIRED " << endl;
+  // printMatrix(equations);
+  // cout << "==========" << endl;
+  // printWordList(halfWordList);
+  // assert(equationCompare(formedEquas, equations));
+  cout << "PASS b=" << b << ", k=" << k << endl;
+  cout << "Done testing Equation formation" << endl;
 }
